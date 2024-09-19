ee6ed7416561ef3fa665d99659bacac9f39c27ea
yagovysk <yago09regis@gmail.com>
2024-09-19 18:33:52 -0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-// https://vitejs.dev/config/
+// Simular o __dirname para usar com ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"), // Usando o __dirname simulado
+		},
+	},
+	build: {
+		rollupOptions: {
+			external: ["dayjs/locale/pt-br"],
+		},
+	},
+});
