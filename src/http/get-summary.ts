77c538d265883125b9fe8b6cb651418dd340bd32
yagovysk <yago09regis@gmail.com>
2024-09-20 13:39:11 -0300

    add

--- src/http/get-summary.ts
+++ src/http/get-summary.ts
@@ -13,32 +13,9 @@ export interface GetSummaryResponse {
 	};
 }
 
-export async function getSummary(): Promise<GetSummaryResponse | null> {
-	try {
-		const response = await fetch(
-			"https://nlw-backend-sigma.vercel.app/summary",
-		);
+export async function getSummary(): Promise<GetSummaryResponse> {
+	const response = await fetch("https://nlw-backend-sigma.vercel.app/summary");
+	const data = await response.json();
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
-		}
-
-		const data = await response.json();
-
-		// Verifique se a estrutura está correta
-		if (
-			data &&
-			data.summary &&
-			typeof data.summary.completed === "number" &&
-			typeof data.summary.total === "number" &&
-			typeof data.summary.goalsPerDay === "object"
-		) {
-			return data;
-		} else {
-			throw new Error("Invalid response structure");
-		}
-	} catch (error) {
-		console.error("Error fetching summary:", error);
-		return null; // Retorne null em caso de erro
-	}
+	return data;
 }
